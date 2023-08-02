c08674c1baaf24d3fa81c4c0fdbf8c6138c632ac
Milind-dev <milinddev1011@gmail.com>
2023-08-02 10:26:34 +0530

    home .html

--- controller/routers.js
+++ controller/routers.js
@@ -66,15 +66,14 @@
 //     return res.json({message:'User has been added successfully'});
 // }
 // })
+
 const express = require("express");
 const router = express.Router();
-
-// const data = require("./data/datas.json");
 const data = require("../data/datas.json");
 
 // Handling request using router
 router.get("/", (req, res, next) => {
-  res.json({ data });
+  res.status(200).json({ data, message:"data succesfully loaded" });
 });
 router.post("/task/addjob", (req, res) => {
   var jsondatas = {
@@ -92,7 +91,9 @@ router.post("/task/addjob", (req, res) => {
   for (let i = 0; i < jobData.length; i++) {
     jobData.push(jsondatas);
     // jobData.push(jsondatas)
-    return res.json({ message: "User has been added successfully" });
+    return res
+      .status(200)
+      .json({ message: "User has been added successfully" });
   }
 });
 
@@ -102,9 +103,9 @@ router.get("/task", (req, res) => {
   console.log(types);
   if (types === "Active_Jobs") {
     let { type } = data;
-    res.json({ type });
+    res.status(200).json({ type, message:"succefully Active jobs type " });
   } else {
-    res.send({ message: "404" });
+    res.send({ message: "404",message:"not found" });
   }
 });
 
@@ -115,27 +116,51 @@ router.get("/task/:name", (req, res) => {
   for (var i = 0; i < jobData.length; i++) {
     if (jobData[i].name === names) {
       // console.log("222222 ",res,jobData[i].name)
-      res.json({ name: jobData[i].name, location: jobData[i].location });
+      res.status(200).json({ name: jobData[i].name, location: jobData[i].location });
     }
   }
-  res.send("error");
+  res.status(404).send("error");
 });
 
-// http://localhost:3000/task/premium?pre=true
-router.get("/task/premium", (req, res) => {
+
+// http://localhost:3000/task/job/premiumsDatas?ispre=true
+router.get("/task/job/premiumsDatas", (req, res) => {
   let { jobData } = data;
-  let arr = [];
-  if (arr.length === null) {
-    return;
+  let ispre = req.query.ispre;
+  console.log(ispre);
+  if(ispre === "false"){
+    const premiumsDatas = jobData.filter((el,index) => el.premium === false)
+    res.json({premiumsDatas,message:"succesffully filter false data"})
   }
-  for (const x in jobData) {
-    let c = jobData[x].name;
-    console.log(c);
-    arr.push(c);
+  else{
+    const premiumsDatas = jobData.filter((el,index) => el.premium === true)
+    res.status(200).json({premiumsDatas,message:"successfully true data filter",status:res.status})
   }
-  res.status(200).json({ arr });
-  // res.send({message:"404"})
 });
+
+
+
+
+
+
+// // http://localhost:3000/task/premium?pre=true
+// router.get("/task/premium", (req, res) => {
+//   let { jobData } = data;
+//   let arr = [];
+//   if (arr.length === null) {
+//     return;
+//   }
+//   for (const x in jobData) {
+//     let c = jobData[x].name;
+//     console.log(c);
+//     arr.push(c);
+//   }
+//   res.status(200).json({ arr });
+//   // res.send({message:"404"})
+// });
+// http://localhost:3000/task/premium?pre=true
+
+
 
 // Importing the router
 module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const data = require("./data/datas.json");
+const path = require('path');
 const bodyParser = require("body-parser");
 const { v4 } = require("uuid");
 const productRouter = require("./controller/routers")
@@ -8,26 +8,47 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+
+
 const middlewares = function (req, res, next) {
-    console.log('middleware addon')
     console.log(`middleware -  ${req.url}  ${req.method} -- ${new Date()}`)
     next()
 }
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(middlewares)
-
 // if (process.env.NODE_ENV !== "production") {
 //     require("dotenv").config();
 // }
 
 // http://localhost:3000/
 app.use("/", middlewares,productRouter);
-app.use("/task/addjob",middlewares,productRouter)
 app.use("/task",middlewares,productRouter)
-app.use("/task/premium",middlewares,productRouter)
+// app.use("/task/premium",middlewares,productRouter)
+// app.use("/task/premium",productRouter)
+app.use("/task/addjob",middlewares,productRouter)
+app.use("/task/job/premiumsDatas",middlewares,productRouter)
+app.get('/', async(req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+// app.get("/task/job/premiumsDatas", (req, res) => {
+//   let { jobData } = data;
+//   let ispre = req.query.ispre;
+//   console.log(ispre);
+//   if(ispre === "false"){
+//     const premiumsDatas = jobData.filter((el,index) => el.premium === false)
+//     res.json({premiumsDatas,message:"succesffully filter data"})
+//   }
+//   else{
+//     const premiumsDatas = jobData.filter((el,index) => el.premium === true)
+//     res.json({premiumsDatas,message:"bi"})
+//   }
+// });
+
 
 app.listen(port, () => {
   console.log(`server start port at  ${port}`);
